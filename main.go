8fcfdc8a36d7d6cdc88505b3d8e3633ad7d629e0
zwz <akazwz@pm.me>
2021-10-23 17:47:51 +0800

    微信小程序登录

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/cache"
 	"github.com/silenceper/wechat/v2/miniprogram/config"
-	"github.com/silenceper/wechat/v2/miniprogram/qrcode"
 	"log"
 )
 
-func main() {
-	wc := wechat.NewWechat()
-	memory := cache.NewMemory()
-	cfg := &config.Config{
-		AppID:     "wxff07eecaebf3238f",
-		AppSecret: "95591fc672b2f6973194865eb7cf8eeb",
-		Cache:     memory,
-	}
+type LoginData struct {
+	Code    string `json:"code"`
+	Encrypt string `json:"encrypt"`
+	Iv      string `json:"iv"`
+}
 
-	mini := wc.GetMiniProgram(cfg)
-	qr := mini.GetQRCode()
-	code, err := qr.CreateWXAQRCode(qrcode.QRCoder{
-		Page:      "",
-		Path:      "./index",
-		Width:     30,
-		Scene:     "",
-		AutoColor: false,
-		LineColor: qrcode.Color{},
-		IsHyaline: false,
+func main() {
+	r := gin.Default()
+	r.POST("/login", func(ctx *gin.Context) {
+		var data LoginData
+		err := ctx.ShouldBindJSON(&data)
+		if err != nil {
+			log.Println("获取数据失败")
+		}
+		wc := wechat.NewWechat()
+		memory := cache.NewMemory()
+		cfg := &config.Config{
+			AppID:     "wx888a82e958faaaaa",
+			AppSecret: "67b8b7ae4d304f3bc44145f820c1ba24",
+			Cache:     memory,
+		}
+		mini := wc.GetMiniProgram(cfg)
+		auth := mini.GetAuth()
+		session, err := auth.Code2Session(data.Code)
+		if err != nil {
+			log.Println("获取 session 错误")
+			return
+		}
+		log.Println("uni:" + session.UnionID)
+		log.Println("open:" + session.OpenID)
+		encryptor := mini.GetEncryptor()
+		plainData, err := encryptor.Decrypt(session.SessionKey, data.Encrypt, data.Iv)
+		if err != nil {
+			log.Println("解密数据错误")
+			return
+		}
+		log.Println(len(plainData.PhoneNumber))
 	})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(string(code))
+	r.Run()
 }
